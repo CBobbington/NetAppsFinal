8c90d257d0d448e00123499d78791ff1e35775b2
CBobbington <erlind.acob@gmail.com>
2015-12-11 02:14:42 -0500

    Fixed small things in Learner

--- Learner.py
+++ Learner.py
@@ -3,6 +3,7 @@ from sklearn import svm
 import shelve
 
 class Learner:
+	# Load data from the shelf and perform initial fit
 	def __init__(self, load_file):
 		if load_file is not None:
 			self._data_file = load_file
@@ -13,18 +14,23 @@ class Learner:
 		if self._data == {}:
 			self._data["data"] = []
 			self._data["class"] = []
+			self._data.sync()
 		
 		self._svm = svm.SVC(probability=True)		
 		if len(self._data["data"]) > 0:
 			self._refit()
 			
-	def add_point(self, dat, cls):
+	# Add data and refit
+	def train(self, dat, cls):
 		self._data["data"].append(dat)
 		self._data["class"].append(cls)
+		self._data.sync()
 		self._refit()
-		
+	
+	# Provide probability of classifications
 	def predict(self, dat):
 		return self._svm.predict(dat)
 	
+	# Helper function for refitting data
 	def _refit(self):
 		self._svm.fit(self._["data"], self._data["class"])
--- README.md
+++ README.md
@@ -7,5 +7,4 @@ python-imaging
 Pika (https://pypi.python.org/pypi/pika)
 Zeroconf (https://pypi.python.org/pypi/zeroconf)
 Adafruit LED Backpack (https://github.com/adafruit/adafruit_python_led_backpack)
-
-
+Scikit (https://pypi.python.org/pypi/scikit-learn/0.17)
